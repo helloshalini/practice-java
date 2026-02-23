ebc0162f2d8b8ffeb2ce04569c1303e370f5cbed
helloshalini <shalinijaiswal1101@gmail.com>
2026-02-23 17:53:39 +0530

    Pattern

---
+++ Pattern/pat1.java
@@ -0,0 +1,23 @@
+public class pat1 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=4; i++) {
+            for(int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        for(int i = 1; i<=4; i++) {
+            for(int j = i; j<=4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
