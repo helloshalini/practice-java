dfdf07a8d6518af0f45f3489a0d6a2980eb38129
helloshalini <shalinijaiswal1101@gmail.com>
2026-02-26 23:58:27 +0530

    Average of a number

---
+++ Pattern/Funcrion/Average.java
@@ -0,0 +1,14 @@
+public class Average {
+
+    public static int avg_of_number(int a, int b, int c) {
+        int avg = (a+b+c)/3;
+
+        return avg;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(avg_of_number(5, 7, 9));
+        
+    }
+}
