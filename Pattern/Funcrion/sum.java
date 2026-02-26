1654676131839e943fb6656986eb0a77c9aecab2
helloshalini <shalinijaiswal1101@gmail.com>
2026-02-26 23:42:50 +0530

    Sum of three numbers

---
+++ Pattern/Funcrion/sum.java
@@ -0,0 +1,15 @@
+public class sum {
+
+    public static int Sum_number(int a, int b, int c) {
+        int sum = a+b+c;
+        return sum;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(Sum_number(4, 5, 1));
+        
+    }
+}
